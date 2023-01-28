708a73b0e3b3e21229657487047a30a88135afbd
M09Ic <m09ician@gmail.com>
2023-01-28 12:59:53 +0800

    重构extract

--- common.go
+++ common.go
@@ -2,7 +2,6 @@ package parsers
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
 )
 
@@ -240,84 +239,4 @@ func mapToString(m map[string]interface{}) string {
 		s += fmt.Sprintf(" %s:%s ", k, v.(string))
 	}
 	return s
-}
-
-type Extracted struct {
-	Name          string   `json:"name"`
-	ExtractResult []string `json:"extract_result"`
-}
-
-func (e *Extracted) String() string {
-	if len(e.ExtractResult) == 1 {
-		if len(e.ExtractResult[0]) > 30 {
-			return fmt.Sprintf("%s:%s ... %d bytes", e.Name, AsciiEncode(e.ExtractResult[0][:30]), len(e.ExtractResult[0]))
-		}
-		return fmt.Sprintf("%s:%s", e.Name, AsciiEncode(e.ExtractResult[0]))
-	} else {
-		return fmt.Sprintf("%s:%d items", e.Name, len(e.ExtractResult))
-	}
-}
-
-type Extracteds []*Extracted
-
-func (es Extracteds) String() string {
-	var s strings.Builder
-	for _, e := range es {
-		s.WriteString("[ " + e.String() + " ]")
-	}
-	return s.String() + " "
-}
-
-type Extractor struct {
-	Name            string           `json:"name"` // extractor name
-	Regexps         []string         `json:"regex"`
-	Tags            []string         `json:"tags"`
-	CompiledRegexps []*regexp.Regexp `json:"-"`
-}
-
-func (e *Extractor) Compile() {
-	e.CompiledRegexps = make([]*regexp.Regexp, len(e.Regexps))
-	for i, r := range e.Regexps {
-		e.CompiledRegexps[i] = regexp.MustCompile(r)
-	}
-}
-
-func (e *Extractor) HasTag(tag string) bool {
-	for _, t := range e.Tags {
-		if t == tag {
-			return true
-		}
-	}
-	return false
-}
-
-func (e *Extractor) Extract(body string) *Extracted {
-	extracts := &Extracted{
-		Name: e.Name,
-	}
-	for _, r := range e.CompiledRegexps {
-		matches := r.FindAllString(body, -1)
-		extracts.ExtractResult = append(extracts.ExtractResult, matches...)
-	}
-	return extracts
-}
-
-type Extractors map[string][]*regexp.Regexp
-
-func (es Extractors) Extract(content string) (extracts []*Extracted) {
-	if len(content) == 0 {
-		return
-	}
-
-	for name, regexps := range es {
-		extracted := &Extracted{
-			Name: name,
-		}
-		for _, r := range regexps {
-			matches := r.FindAllString(content, -1)
-			extracted.ExtractResult = append(extracted.ExtractResult, matches...)
-		}
-		extracts = append(extracts, extracted)
-	}
-	return extracts
 }
